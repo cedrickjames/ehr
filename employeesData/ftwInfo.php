ec773951cc910a89df72a6e9165eebd8dc3d3613
cedrickjames <cedrickorozo@gmail.com>
2025-03-03 14:50:13 +0800

    add delete ftw

--- employeesData/ftwInfo.php
+++ employeesData/ftwInfo.php
@@ -6,6 +6,19 @@ if (isset($_GET['rf'])) {
     $idNumber = "not found";
 }
 
+if(isset($_POST['deleteFitToWorkRecord'])){
+    $id = $_POST['ftwidtodelete'];
+    $sql = "DELETE FROM `fittowork` WHERE `id` = '$id'";
+    $results = mysqli_query($con, $sql);
+   
+    if ($results) {
+      echo "<script>alert('Delete successful.');</script>";
+    } else {
+      echo "<script>alert('There is a problem with deleting record. Please contact your administrator.');</script>";
+    }
+  }
+
+
 ?>
 <div class="text-[9px] 2xl:text-lg mb-5">
     <p class="mb-2"><span class=" self-center text-md font-semibold whitespace-nowrap   text-[#193F9F]">Fit to Work History</span></p>
@@ -43,7 +56,7 @@ if (isset($_GET['rf'])) {
                     <tbody>
                         <?php
                         $ftwNo = 1;
-                        $sql = "SELECT * FROM `fittowork` WHERE `idNumber` = '$idNumber' ORDER BY `id` ASC; 
+                        $sql = "SELECT * FROM `fittowork` WHERE `idNumber` = '$idNumber' ORDER BY `id` DESC; 
                     ";
                         $result = mysqli_query($con, $sql);
                         while ($row = mysqli_fetch_assoc($result)) {
@@ -52,8 +65,11 @@ if (isset($_GET['rf'])) {
 
                             <tr>
                                 <td> <?php echo $ftwNo; ?> </td>
-                                <td>
+                                <td class="flex gap-2">
+
                                     <button type="button" onclick="showData(<?php echo $row['id']; ?>)" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Show</button>
+                                    <button type="button" onclick="askdelete(<?php echo $row['id']; ?>)" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Delete</button>
+                               
                                 </td>
                                 <td> <?php echo $row['date']; ?> </td>
                                 <td> <?php echo $row['time']; ?> </td>
@@ -127,7 +143,56 @@ if (isset($_GET['rf'])) {
 </div>
 
 
+<div id="deleteFTWRecord" tabindex="-1" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+    <div class="relative p-4 w-full max-w-md max-h-full">
+        <div class="relative bg-white rounded-lg shadow-sm dark:bg-gray-700">
+            <button type="button" class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="deleteFTWRecord">
+                <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                </svg>
+                <span class="sr-only">Close modal</span>
+            </button>
+            <div class="p-4 md:p-5 text-center">
+                <svg class="mx-auto mb-4 text-gray-400 w-12 h-12 dark:text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11V6m0 8h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+                </svg>
+                <form action="" method="POST">
+                <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this record?</h3>
+                <input type="text" id="ftwidtodelete" name="ftwidtodelete" class="hidden">
+                <button  type="submit" name="deleteFitToWorkRecord" id="deleteFTWRecord" class=" text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
+                    Yes, I'm sure
+                </button>
+               
+                <button data-modal-hide="deleteFTWRecord" type="button" class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">No, cancel</button>
+                </form>
+              
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
 <script>
+    const $targetDeactivateUser = document.getElementById('deleteFTWRecord');
+  const deactivateModal = {
+    placement: 'center-center',
+    backdrop: 'static',
+    backdropClasses: 'bg-gray-900 bg-opacity-50 dark:bg-opacity-80 fixed inset-0 z-40',
+    closable: true,
+    onHide: () => {
+    },
+    onShow: () => {
+
+    },
+    onToggle: () => {
+    }
+  };
+  const modaldeactivate = new Modal($targetDeactivateUser, deactivateModal);
+
+
+
     function showData(consultId) {
 
         var currentUrl = window.location.href; // Get the current URL
@@ -141,5 +206,12 @@ if (isset($_GET['rf'])) {
             console.error('The "rf" parameter is not present in the current URL.');
         }
 
+    }
+
+
+    function askdelete(id){
+
+        document.getElementById("ftwidtodelete").value = id;
+        modaldeactivate.toggle();
     }
 </script>
