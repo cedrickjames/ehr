8bf5c91f87962d54b4dd922bf184c4a441d21ffc
cedrickjames <cedrickorozo@gmail.com>
2025-04-11 08:29:09 +0800

    fix some errors

--- bloodChem/directEmployees.php
+++ bloodChem/directEmployees.php
@@ -1333,7 +1333,7 @@ if (isset($_POST['addBloodChemImport'])) {
         </div>
     </div>
 </div>
-<div id="export" tabindex="-1" aria-hidden="true" class=" hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
+<div id="exportModal" tabindex="-1" aria-hidden="true" class=" hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
     <div class="relative p-4 w-full max-w-2xl max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -1342,7 +1342,7 @@ if (isset($_POST['addBloodChemImport'])) {
                 <h3 class=" font-semibold text-gray-900 dark:text-white">
                     Export HLP Record
                 </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="export">
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-toggle="exportModal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
@@ -1540,13 +1540,13 @@ if (isset($_POST['addBloodChemImport'])) {
                                         <label class="block  my-auto font-semibold text-gray-900 ">Choose quantity:</label>
                                         <div class="flex relative ">
                                             <div class="relative flex items-center max-w-[8rem]">
-                                                <button type="button" id="decrement-button" data-input-counter-decrement="quantityMeds1" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-9 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                                <button type="button" id="decrement-button-edit" data-input-counter-decrement="quantityMeds1" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-s-lg p-3 h-9 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                                                     <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 2">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h16" />
                                                     </svg>
                                                 </button>
                                                 <input type="text" name="cnsltnMedsQuantity" id="quantityMeds1" data-input-counter data-input-counter-min="1" data-input-counter-max="50" aria-describedby="helper-text-explanation" class="bg-gray-50 border-x-0 border-gray-300 h-9 text-center text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full py-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="999" value="1" />
-                                                <button type="button" id="increment-button" data-input-counter-increment="quantityMeds1" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-9 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
+                                                <button type="button" id="increment-button-edit" data-input-counter-increment="quantityMeds1" class="bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:border-gray-600 hover:bg-gray-200 border border-gray-300 rounded-e-lg p-3 h-9 focus:ring-gray-100 dark:focus:ring-gray-700 focus:ring-2 focus:outline-none">
                                                     <svg class="w-3 h-3 text-gray-900 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                                                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 1v16M1 9h16" />
                                                     </svg>
@@ -1554,7 +1554,7 @@ if (isset($_POST['addBloodChemImport'])) {
 
                                             </div>
 
-                                            <button type="button" id="addmedsbtn" onclick="addSelectedValue1(document.getElementById('nameOfMedicine1').value, document.getElementById('quantityMeds1').value)" class="ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  p-2 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Add to list
+                                            <button type="button" id="editaddmedsbtn" onclick="addSelectedValue1(document.getElementById('nameOfMedicine1').value, document.getElementById('quantityMeds1').value)" class="ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg  p-2 text-center inline-flex items-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"> Add to list
                                             </button>
                                         </div>
                                     </div>
@@ -1655,69 +1655,7 @@ if (isset($_POST['addBloodChemImport'])) {
         </div>
     </div>
 </div>
-<div id="exportBloodChem" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative w-full max-w-md max-h-full">
-        <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-            <button type="button" data-modal-toggle="exportBloodChem" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
-                <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-                <span class="sr-only">Close modal</span>
-            </button>
-            <div class="px-6 py-6 lg:px-8">
-                <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Export HLP</h3>
-                <form class="space-y-6" action="" method="POST">
-                    <div>
-                        <label for="employer" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Employer</label>
 
-                        <select id="employer" name="employer" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-
-                            <option selected value="GPI">GPI</option>
-                            <option value="All">All</option>
-                            <option value="Maxim">Maxim</option>
-                            <option value="Powerlane">Powerlane</option>
-                            <option value="Nippi">Nippi</option>
-                            <option value="Mangreat">Mangreat</option>
-                            <option value="Otrelo">Otrelo</option>
-                            <option value="Canteen">Canteen</option>
-                            <option value="Alarm">Alarm</option>
-
-                        </select>
-                        <label for="month" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Month</label>
-
-                        <select id="month" name="month" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-
-                            <?php
-                            $date = new DateTime('01-01-2023');
-                            $dateNow = new DateTime();
-                            $monthNow = $dateNow->format('F');
-
-                            for ($i = 1; $i <= 12; $i++) {
-                                $month = $date->format('F');
-                            ?> <option <?php if ($monthNow == $month) {
-                                            echo "selected";
-                                        } ?> value="<?php echo $month; ?>"><?php echo $month; ?></option> <?php
-                                                                                                            $date->modify('next month');
-                                                                                                        }
-                                                                                                            ?>
-                        </select>
-                        <label for="year" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Year</label>
-                        <input type="number" value="<?php $dateNow2 = new DateTime();
-                                                    $year = $dateNow2->format('Y');
-                                                    echo $year; ?>" name="year" id="year" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
-                    </div>
-
-                    <button type="submit" name="" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                        Generate Excel
-                    </button>
-
-
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
 
 <script src="../node_modules/jquery/dist/jquery.min.js"></script>
 <script>
@@ -1764,7 +1702,30 @@ else{
 });
 
 
+const exportBloodChem = document.getElementById('exportModal');
 
+// options with default values
+const exportBloodChems = {
+
+    backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 relative inset-0 z-40',
+    closable: true,
+    onHide: () => {
+        console.log('modal is hidden');
+    },
+    onShow: () => {
+        console.log('modal is shown');
+    },
+    onToggle: () => {
+        console.log('modal has been toggled');
+    },
+};
+
+const modalExport = new Modal(exportBloodChem, exportBloodChems);
+
+function openExportModal(element) {
+    modalExport.toggle();
+
+};
 
 
 const $targetPromptModal = document.getElementById('askFirst');
@@ -2321,30 +2282,7 @@ $('#cnsltnBloodChem').val(filteredFindings);
         modalImport.toggle();
 
     };
-    const exportBloodChem = document.getElementById('export');
 
-    // options with default values
-    const exportBloodChems = {
-
-        backdropClasses: 'bg-gray-900/50 dark:bg-gray-900/80 relative inset-0 z-40',
-        closable: true,
-        onHide: () => {
-            console.log('modal is hidden');
-        },
-        onShow: () => {
-            console.log('modal is shown');
-        },
-        onToggle: () => {
-            console.log('modal has been toggled');
-        },
-    };
-
-    const modalExport = new Modal(exportBloodChem, exportBloodChems);
-
-    function openExportModal(element) {
-        modalExport.toggle();
-
-    };
 
 
     const addBloodChem = document.getElementById('addBloodChem');
--- bloodChem/navbar.php
+++ bloodChem/navbar.php
@@ -70,7 +70,7 @@ if (isset($_POST['submitNewPassword'])) {
 <nav class=" top-0 text-sm 2xl:text-xl border-gray-200 ">
   <div class="w-screen flex flex-wrap items-center justify-between mx-auto px-4 2xl:px-auto p-1 2xl:p-4">
   <div class="flex">
-      <span id="sidebarButton" type="button" data-drawer-target="drawer-sidebar" data-drawer-show="drawer-sidebar" aria-controls="drawer-sidebar" class="block lg:hidden mx-10 ">
+      <span id="sidebarButton" type="button"  class="block lg:hidden mx-10 ">
         <!-- <i class="fa-solid fa-bars fa-lg"></i>  -->
 
       </span>
@@ -124,73 +124,6 @@ if (isset($_POST['submitNewPassword'])) {
 
 
 
-<!-- drawer component -->
-<div id="drawer-navigation" class="fixed top-0 left-0 z-40 h-screen p-4 overflow-y-auto transition-transform -translate-x-full bg-white w-96 " tabindex="-1" aria-labelledby="drawer-navigation-label">
-  <div class="mb-5">
-    <h5 id="drawer-navigation-label" class="text-base font-semibold text-gray-500 uppercase ">Helpdesk</h5>
-    <button type="button" data-drawer-hide="drawer-navigation" aria-controls="drawer-navigation" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 absolute top-2.5 right-2.5 inline-flex items-center justify-center ">
-      <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
-        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
-      </svg>
-      <span class="sr-only">Close menu</span>
-    </button>
-
-  </div>
-
-
-  <!-- <button type="button"onclick="shows()"  class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 absolute top-2.5 right-2.5 inline-flex items-center   >
-          <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-          <span class="sr-only">Close menu</span>
-      </button> -->
-  <div class="py-5 pr-5 overflow-y-auto">
-    <ul class="space-y-2">
-      <li>
-        <a href="index.php" id="sidehome1" class="  flex items-center p-4 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
-          <!-- <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 -white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg> -->
-          <i class="fa-solid fa-house"></i>
-          <span class="ml-3">Home</span>
-        </a>
-      </li>
-      <li>
-        <a href="myRequest.php" id="sideMyRequest1" class="  flex items-center p-4 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
-          <!-- <svg aria-hidden="true" class="w-6 h-6 text-gray-500 transition duration-75  group-hover:text-gray-900 -white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg> -->
-          <i class="fa-solid fa-ticket"></i>
-          <span class="ml-3">My Request</span>
-        </a>
-      </li>
-      <li>
-        <a href="history.php" id="sidehistory1" class="  flex items-center p-4 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
-
-          <i class="fa-solid fa-clock-rotate-left"></i> <span class="flex-1 ml-3 whitespace-nowrap">History</span>
-        </a>
-      </li>
-      <li>
-        <a href="user.php" id="sideuser1" class="   flex items-center p-4 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
-
-          <i class="fa-solid fa-user"></i> <span class="flex-1 ml-3 whitespace-nowrap">User</span>
-        </a>
-      </li>
-      <li>
-        <a href="pms.php" id="sidepms1" class="  flex items-center p-4 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
-
-          <i class="fa-solid fa-broom"></i> <span class="flex-1 ml-3 whitespace-nowrap">PMS</span>
-        </a>
-      </li>
-      <li>
-        <a href="devices.php" id="sidedevice1" class="  flex items-center p-4 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
-
-          <i class="fa-solid fa-computer"></i> <span class="flex-1 ml-3 whitespace-nowrap">Devices</span>
-        </a>
-      </li>
-      <li>
-        <a href="documents.php" id="sidedocs1" class="flex items-center p-4 text-base font-normal text-gray-900 rounded-lg  hover:bg-gray-100 ">
-
-          <i class="fa-solid fa-file"></i> <span class="flex-1 ml-3 whitespace-nowrap">Documents</span>
-        </a>
-      </li>
-    </ul>
-  </div>
-</div>
 <!-- side bar drawer component -->
 <div id="sidebar" class="text-xs 2xl:text-xl hidden lg:block  fixed top-16 left-0 z-40 h-screen pl-4 pr-0 overflow-y-auto transition-transform    transform-none" tabindex="-1" aria-labelledby="sidebar-label" aria-modal="true" role="dialog">
 
@@ -572,81 +505,6 @@ while ($row = mysqli_fetch_assoc($result)) {
   </div>
 </div>
 
-<div id="registerModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-  <div class="relative w-full max-w-md max-h-full">
-    <!-- Modal content -->
-    <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-      <button type="button" data-modal-toggle="registerModal" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
-        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-          <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-        </svg>
-        <span class="sr-only">Close modal</span>
-      </button>
-      <div class="px-6 py-6 lg:px-8">
-        <h3 class="mb-4 text-xl font-medium text-gray-900 dark:text-white">Register</h3>
-        <form class="space-y-6" action="" method="POST">
-          <div>
-            <div>
-              <label for="year" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Employee ID</label>
-              <input type="text" name="userEmployeeId" id="userEmployeeId" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
-            </div>
-            <div>
-
-              <label for="year" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-              <input type="text" name="userFullName" id="fullName" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
-            </div>
-            <div>
-              <label for="year" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-              <input type="email" name="userEmail" id="userEmail" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required>
-            </div>
-            <label for="userDepartment" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Department</label>
-
-            <select id="userDepartment" name="userDepartment" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-
-              <?php
-
-              $sql = "SELECT DISTINCT TRIM(department) AS department FROM users;";
-              $result = mysqli_query($con, $sql);
-
-              while ($row = mysqli_fetch_assoc($result)) {
-              ?> <option value="<?php echo $row['department']; ?>"><?php echo $row['department']; ?></option> <?php
-                                                                                                            }
-
-                                                                                                              ?>
-            </select>
-            <label for="month" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Company</label>
-
-<select id="company" name="company" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-  <option selected value="GPI">GPI</option>
-  <option value="Maxim">Maxim</option>
-  <option value="Nippi">Nippi</option>
-  <option value="Powerlane">Powerlane</option>
-
-
-            <label for="month" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">User Type</label>
-
-            <select id="userType" name="userType" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-              <option selected value="nurse">Nurse</option>
-              <option value="head">Department Head</option>
-              <option value="hr">HR</option>
-              <option value="doctor">Doctor</option>
-
-
-
-            </select>
-
-          </div>
-
-
-          <button type="submit" name="registerUser" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            Register
-          </button>
-
-        </form>
-      </div>
-    </div>
-  </div>
-</div>
 
 <div id="changePassword" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
   <div class="relative w-full max-w-md max-h-full">
