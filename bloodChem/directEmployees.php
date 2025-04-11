b6a2d550bd03a453b1306d0efd9a2e5c8119f26b
cedrickjames <cedrickorozo@gmail.com>
2025-04-11 08:35:44 +0800

    change addbloodchem id

--- bloodChem/directEmployees.php
+++ bloodChem/directEmployees.php
@@ -679,7 +679,7 @@ if (isset($_POST['addBloodChemImport'])) {
 </div>
 
 
-<div id="addBloodChem" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full  bg-gray-900/50 dark:bg-gray-900/80 ">
+<div id="addBloodChemModal" data-modal-backdrop="static" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full  bg-gray-900/50 dark:bg-gray-900/80 ">
     <div class="relative p-4 w-full max-w-6xl max-h-full">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -688,7 +688,7 @@ if (isset($_POST['addBloodChemImport'])) {
                 <h3 class=" font-semibold text-gray-900 dark:text-white">
                     Add HLP Record
                 </h3>
-                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="addBloodChem">
+                <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="addBloodChemModal">
                     <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" />
                     </svg>
@@ -1661,6 +1661,9 @@ if (isset($_POST['addBloodChemImport'])) {
 <script>
 
 
+
+
+
 $("#interventionSelect").change(function() {
 
 if ($(this).val() === "Clinic Rest Only" || $(this).val() === "Medication, Clinic Rest and Medical Consultation") {
@@ -2285,7 +2288,7 @@ $('#cnsltnBloodChem').val(filteredFindings);
 
 
 
-    const addBloodChem = document.getElementById('addBloodChem');
+    const addBloodChemModal = document.getElementById('addBloodChemModal');
 
     // options with default values
     const addBloodChems = {
@@ -2303,7 +2306,7 @@ $('#cnsltnBloodChem').val(filteredFindings);
         },
     };
 
-    const modalAdd = new Modal(addBloodChem, addBloodChems);
+    const modalAdd = new Modal(addBloodChemModal, addBloodChems);
 
     function openAddModal(element) {
         modalAdd.toggle();
